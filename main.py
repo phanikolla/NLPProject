d0b84655e543c2348b02588b3f898e20e4f8117e
Phani Kumar Kolla <phani.kollas@gmail.com>
2023-03-12 09:18:02 +0530

    updated Key

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 # load the sentiment model
 #
-openai.api_key = "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"
+openai.api_key = "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"
 def generate_sql_query(prompt, max_tokens=1024, temperature=0, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0):
     response = openai.Completion.create(
         #engine="davinci-codex",

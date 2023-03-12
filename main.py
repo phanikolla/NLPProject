0d8e34953ea6f1dcefd9553457a32e2f48d3d382
Phani Kumar Kolla <phani.kollas@gmail.com>
2023-03-12 08:50:42 +0530

    updates to query

--- main.py
+++ main.py
@@ -16,15 +16,25 @@ app = FastAPI(
 openai.api_key = "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"
 def generate_sql_query(prompt, max_tokens=1024, temperature=0, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0):
     response = openai.Completion.create(
-        model="curie:ft-personal:customercomplaint-26-02-2023-2023-02-26-07-27-52",
-        prompt=prompt,
-        max_tokens=1024,
+        #engine="davinci-codex",
+        engine="text-davinci-002",
+        #engine="davinci-instruct-beta-v3",
+        #model="curie:ft-personal:customercomplaint-10-03-2023-2023-03-10-06-47-35",
+        ##prompt="### Postgres SQL tables, with their properties:\n#\n# Consumercomplaints(Complaint_ID, Company, Product,Date_received)\n#\n### A query to list the companies and the count of complaints from each company\n",
+        ##prompt="### Postgres SQL tables, with their properties:\n#\n# Consumercomplaints(Complaint_ID, Company, product_key,Date_received,State,Submitted_via)\n# product_mstr(product_key,product_name,product_desc,create_date,updated_date)\n# sub_prod_mstr(subproduct_key,subproduct_name,subproduct_desc,created_dt,updated_dt)\n# prod_mapping(product_key,subproduct_key,mapping_id,created_dt,updated_dt) \n#\n##"+ prompt +"\n#",
+        prompt="### Postgres SQL tables, with their properties:\n#\n# Consumercomplaints(Complaint_ID, Company, prod_subprod_mapping_key ,Date_received,State,Submitted_via,Timely_response)\n# Product_master(product_ID,product_name,product_desc,created_date,updated_date)\n# sub_prod_master(subproduct_ID,subproduct_name,subproduct_desc,created_dt,updated_dt)\n# prod_mapping(product_ID,subproduct_ID,prod_subprod_mapping_key,created_dt,updated_dt) \n#\n##"+ prompt +"\n#",
+        ##prompt="### Postgres SQL tables, with their properties:\n#\n# Consumercomplaints(Date_received,mapping_id,Issue,Sub_issue,Consumer_complaint_narrative,Company_public_response,Company,State,ZIP_code,Tags,Consumer_consent_provided,Submitted_via,Date_sent_to_company,Company_response_to_consumer,Timely_response,Consumer_disputed,Complaint_ID)\n# Product_master(product_ID,product_name,product_desc,created_date,updated_date)\n# sub_prod_master(subproduct_ID,subproduct_name,subproduct_desc,created_date,updated_dt)\n# prod_mapping(product_ID,subproduct_ID,mapping_id,created_date,updated_date) \n#\n### Return total number of complaints for Consumer Loan product?",
+        ##prompt="### Postgres SQL tables, with their properties:\n#\n# Consumercomplaints(Complaint_ID, Company, product,subproduct,Date_received,State,Submitted_via,Timely_response)"+ prompt +"\n#",
+      
+        max_tokens=max_tokens,
         temperature=temperature,
         top_p=top_p,
         n=1,
         frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty)
-    return response.choices[0].text
+        presence_penalty=presence_penalty,
+        stop=[";"]
+    )
+    return response.choices[0].text.strip()
 
 
 def query_database(prompt):
